5126da28c98113d9aa0acbbfb7ac15e5d9abfc12
Matthew McAlarney <67719286+MacCode7110@users.noreply.github.com>
2022-02-22 00:44:33 -0500

    Update scan.c

--- scan.c
+++ scan.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : OperatingSystemsProject5Code.c
- Author      : Matthew McAlarney
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
 #include <assert.h>
 #include <stdio.h>
 #include <math.h>
@@ -15,12 +5,20 @@
 #include <semaphore.h>
 #include <stdlib.h>
 #include <string.h>
+#include <pthread.h>
+#include <assert.h>
 #define MAX_LINE_SIZE 256
+
+//Fully compiles
 
 //Globals:
 
 int *input; //shared resource among all threads
+int n = 0; //shared resource among all threads
+int numThreads = 0; //shared resource among all threads
+pthread_mutex_t lock; //Declare the mutex for locking purposes
 
+//Read in input from the argument file into an array of integer pointers
 void read_input_vector(const char* filename, int n, int* array)
 {
   FILE *fp;
@@ -44,40 +42,84 @@ void read_input_vector(const char* filename, int n, int* array)
   fclose(fp);
 }
 
-void calculatePrefixSum()
+//Calculate the prefix sum, which contains the critical section
+void* calculatePrefixSum(int n)
 {
+	//Provide a lock to the current thread that has entered the critical section so that no other locks can enter at the same time
+	pthread_mutex_lock(&lock);
+
 	//Critical Section
 	for(int i = 1; i < n; i++)
 	{
 		input[i] += input[i-1];
 	}
+
+	//Release the lock so that other threads now have a chance to enter the critical section:
+	pthread_mutex_unlock(&lock);
+
+	return NULL;
 }
 
-void printPrefixSum(int n)
+//Print the prefix sum
+void* printPrefixSum()
 {
 	for(int i = 0; i < n; i++)
 	{
 		printf("%d\n", input[i]);
 	}
+	return NULL;
 }
 
 int main(int argc, char* argv[])
 {
   //argv[0] is a pointer to the name of the program being run.
   char* filename = argv[1];//filename pointer
-  int n = atoi(argv[2]); //size of the input vector = number of lines in the file
-  int numThreads = argv[3]; //Third argument specifies number of threads to use for computing the solution
+  n = atoi(argv[2]); //size of the input vector = number of lines in the file
+  numThreads = atoi(argv[3]); //Third argument specifies number of threads to use for computing the solution
+  pthread_t threadlist[numThreads]; //A list of all threads sent into the command line argument
 
   if(n < 2) //If there are less than two numbers for the prefix sum, then we cannot compute, and so we need to exit with EXIT_FAILURE.
   {
 	exit(EXIT_FAILURE);
   }
 
-  input = malloc(sizeof(int) * n); //Used to read in the contents (integers in this case) of the first argument file in the read_input_vector function
+  //Read in the contents (integers in this case) of the first argument file in the read_input_vector function
+  input = malloc(sizeof(int) * n);
   read_input_vector(filename, n, input);
 
-  //Print the prefix sum
-  printPrefixSum(n);
+  //Calculate the prefix sum
 
+  //Initialize the mutex using its virtual address
+  pthread_mutex_init(&lock, NULL);
+
+  //Create each thread using a loop:
+  for(int x = 0; x < sizeof(threadlist); x++)
+  {
+	  int result = pthread_create(&threadlist[x], NULL, calculatePrefixSum(n), NULL);
+
+	  if(result != 0)
+	  {
+		  printf("\nThread cannot be created : [%s]", strerror(result));
+	  }
+  }
+
+  //Block the calling thread until all threads created for the prefix_sum calculation terminate:
+  for(int y = 0; y < sizeof(threadlist); y++)
+  {
+	  pthread_join(threadlist[y], NULL);
+  }
+
+  //Destroy the mutex lock since all threads created for the prefix_sum calculation have terminated:
+  pthread_mutex_destroy(&lock);
+
+  //Print the Prefix Sum:
+
+  pthread_t thread_id;
+  pthread_create(&thread_id, NULL, printPrefixSum, NULL);
+  //Block the calling thread until thread_id terminates. Then, the calling thread can resume execution.
+  pthread_join(thread_id, NULL);
+
+  exit(0);
+  fflush(stdout);
   return EXIT_SUCCESS;
 }
